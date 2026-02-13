50d61df085e680deda3e228f02ade6775e902ada
tommasovenza <tommasovenza@gmail.com>
2026-02-13 17:42:46 +0100

    created a function Book that prints some books item

---
+++ object-constructors/constructor.js
@@ -0,0 +1,29 @@
+// Write a constructor for making “Book” objects.
+// We will revisit this in the next project.
+// Your book objects should have the book’s title, author, the number of pages, and whether or not
+// you have read the book.
+
+function Book(title, author, pages, read) {
+  // throw an error
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor")
+  }
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${read === false ? "not read yet!" : "just read!"}`
+  }
+}
+
+const firstBook = new Book("50 Sfumature di Grigio", "Unknown", 400, false)
+const secondBook = new Book("L'occhio del Lupo", "Daniel Pennac", 180, true)
+// const wrong = Book("this is wrong", "wrong author", 300, false)
+
+console.log(firstBook)
+console.log(secondBook)
+console.log(firstBook.info())
+console.log(secondBook.info())
+// console.log(wrong)
